da5e05838d2cebed6c8f74d166392ce81c4ed10c
Elvis Ahmetovic <e.ahmetovic@Elviss-MacBook-Air.local>
2025-08-27 14:46:34 +0200

    fix deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/Camper-rent/' : '/',
+    base: '/Camper-rent/',
     plugins: [
         vue(),
         tailwindcss(),
